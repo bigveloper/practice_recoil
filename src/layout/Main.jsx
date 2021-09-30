1c17b45d4ffe16098be54d2e78efebaa7395ff48
bigveloper <bigveloper@gmail.com>
2021-09-30 19:36:39 +0900

    recoil

---
+++ src/layout/Main.jsx
@@ -0,0 +1,19 @@
+import Logout from '../pages/Logout';
+
+/**
+ * @description Main
+ */
+
+function Main() {
+    const style = {
+        display: 'flex',
+    };
+
+    return (
+        <>
+            <Logout />
+        </>
+    );
+}
+
+export default Main;
---
+++ src/pages/Logout.jsx
@@ -0,0 +1,25 @@
+import { useRecoilState } from 'recoil';
+import { loginState } from '../global/loginState';
+
+/**
+ * @description Logout 컴포넌트
+ */
+
+function Logout() {
+    // recoil
+    const [isLogin, setIsLogin] = useRecoilState(loginState);
+
+    // event
+    const onLogout = () => setIsLogin(false);
+
+    // view
+    return (
+        <div>
+            <form>
+                <button onClick={onLogout}>Logout</button>
+            </form>
+        </div>
+    );
+}
+
+export default Logout;
